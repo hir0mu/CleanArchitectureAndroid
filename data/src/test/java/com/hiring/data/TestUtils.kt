a7d76d3a1af6d3a48627c5b9fe8f582e2a27e2ff
hiromu21 <hiring.y@gmail.com>
2021-03-03 19:15:49 +0900

    Create api test

---
+++ data/src/test/java/com/hiring/data/TestUtils.kt
@@ -0,0 +1,18 @@
+package com.hiring.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+object TestUtils {
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .build()
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl("https://qiita.com/api/v2/")
+        .addConverterFactory(Json.asConverterFactory(checkNotNull(MediaType.parse("application/json"))))
+        .build()
+}
