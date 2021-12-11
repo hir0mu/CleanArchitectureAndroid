1ec24158c72771d3251a4dce9d81455ce66e9cfc
hiromu21 <hiring.y@gmail.com>
2021-12-11 14:59:38 +0900

    Remove withContext from repository

--- data/src/main/java/com/hiring/data/repository/impl/FavoriteRepositoryImpl.kt
+++ data/src/main/java/com/hiring/data/repository/impl/FavoriteRepositoryImpl.kt
@@ -3,33 +3,23 @@ package com.hiring.data.repository.impl
 import com.hiring.data.db.ArticleDao
 import com.hiring.data.entity.FavArticle
 import com.hiring.data.repository.FavoriteRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class FavoriteRepositoryImpl(
     private val articleDao: ArticleDao
 ) : FavoriteRepository {
     override suspend fun getAll(): List<FavArticle> {
-        return withContext(Dispatchers.IO) {
-            articleDao.getAll()
-        }
+        return articleDao.getAll()
     }
 
     override suspend fun getArticlesByIds(articleIds: List<String>): List<FavArticle> {
-        return withContext(Dispatchers.IO) {
-            articleDao.getArticlesByIds(articleIds)
-        }
+        return articleDao.getArticlesByIds(articleIds)
     }
 
     override suspend fun insertAll(vararg articles: FavArticle) {
-        return withContext(Dispatchers.IO) {
-            articleDao.insertAll(articles.toList())
-        }
+        return articleDao.insertAll(articles.toList())
     }
 
     override suspend fun deleteByArticleId(articleId: String) {
-        return withContext(Dispatchers.IO) {
-            articleDao.deleteByArticleId(articleId)
-        }
+        return articleDao.deleteByArticleId(articleId)
     }
 }
