866fb672d71d5cc40ff5cf469683eb8149931923
hiromu21 <hiring.y@gmail.com>
2021-12-27 11:42:43 +0900

    Open detail form fav

--- app/src/main/java/com/hiring/cleanarchitecture/view/favorite/FavoriteItem.kt
+++ app/src/main/java/com/hiring/cleanarchitecture/view/favorite/FavoriteItem.kt
@@ -3,11 +3,22 @@ package com.hiring.cleanarchitecture.view.favorite
 import com.hiring.cleanarchitecture.databinding.ItemFavoriteArticleBinding
 import com.hiring.cleanarchitecture.domain.model.ArticleModel
 import com.hiring.cleanarchitecture.util.SimpleItem
+import com.hiring.cleanarchitecture.view.list.ArticleItem
 
 class FavoriteItem(
-    private val article: ArticleModel
+    private val article: ArticleModel,
+    private val onItemClick: (ArticleModel) -> Unit
 ): SimpleItem<ItemFavoriteArticleBinding>() {
     override fun bind(binding: ItemFavoriteArticleBinding) {
         binding.article = article
+        binding.root.setOnClickListener { onItemClick(article) }
+    }
+
+    override fun isSameAs(other: SimpleItem<*>): Boolean {
+        return (other is FavoriteItem) && other.article.id == article.id && other.article.isFavorite == article.isFavorite
+    }
+
+    override fun hasSameContentAs(other: SimpleItem<*>): Boolean {
+        return (other is FavoriteItem) && other.article.id == article.id
     }
 }
