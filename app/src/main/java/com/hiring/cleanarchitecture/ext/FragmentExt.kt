c83254f6b6e1ed5ddfb277926c1597578c1bccfd
hiromu21 <hiring.y@gmail.com>
2022-03-19 17:05:35 +0900

    Create common function for showing snackbar

--- app/src/main/java/com/hiring/cleanarchitecture/ext/FragmentExt.kt
+++ app/src/main/java/com/hiring/cleanarchitecture/ext/FragmentExt.kt
@@ -4,6 +4,9 @@ import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.hiring.cleanarchitecture.R
+import com.hiring.cleanarchitecture.view.Failure
 
 fun Fragment.setupToolbar(toolbar: Toolbar, @StringRes title: Int, hasOptionMenu: Boolean = false) {
     (requireActivity() as? AppCompatActivity)?.apply {
@@ -11,4 +14,11 @@ fun Fragment.setupToolbar(toolbar: Toolbar, @StringRes title: Int, hasOptionMenu
         setTitle(title)
     }
     setHasOptionsMenu(hasOptionMenu)
+}
+
+fun Fragment.showErrorSnackBar(failure: Failure) {
+    val root = view ?: return
+    Snackbar.make(root, failure.message, Snackbar.LENGTH_INDEFINITE)
+        .setAction(R.string.retry) { failure.retry() }
+        .show()
 }
