8577b167d415c403734421575cc92ca15251422f
hiromu21 <hiring.y@gmail.com>
2022-03-19 17:16:59 +0900

    Remove unused codes

--- app/src/main/java/com/hiring/cleanarchitecture/ext/BindingAdapters.kt
+++ app/src/main/java/com/hiring/cleanarchitecture/ext/BindingAdapters.kt
@@ -5,16 +5,12 @@ import android.webkit.WebView
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.TransitionOptions
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.DrawableCrossFadeTransition
-import com.bumptech.glide.request.transition.Transition
-import com.bumptech.glide.request.transition.TransitionFactory
-import javax.xml.transform.TransformerFactory
 
 object BindingAdapters {
     @JvmStatic
--- data/src/main/java/com/hiring/data/db/Converters.kt
+++ data/src/main/java/com/hiring/data/db/Converters.kt
@@ -1,33 +1,17 @@
 package com.hiring.data.db
 
 import androidx.room.TypeConverter
-import com.hiring.data.entity.ArticleGroup
-import com.hiring.data.entity.ArticleTag
 import com.hiring.data.entity.User
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+@ExperimentalSerializationApi
 class UserConverters {
     @TypeConverter
     fun toUser(json: String): User = Json.decodeFromString(json)
 
     @TypeConverter
     fun toString(user: User): String = Json.encodeToString(user)
-}
-
-class ArticleGroupConverters {
-    @TypeConverter
-    fun toArticleGroup(json: String): ArticleGroup = Json.decodeFromString(json)
-
-    @TypeConverter
-    fun toString(articleGroup: ArticleGroup): String = Json.encodeToString(articleGroup)
-}
-
-class ArticleTagConverters {
-    @TypeConverter
-    fun toUser(json: String): ArticleTag = Json.decodeFromString(json)
-
-    @TypeConverter
-    fun toString(articleTag: ArticleTag): String = Json.encodeToString(articleTag)
 }
