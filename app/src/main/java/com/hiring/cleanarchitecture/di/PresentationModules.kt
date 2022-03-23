e60d4dcfbdf3107edae81826d7484b01006ced36
hiromu21 <hiring.y@gmail.com>
2022-03-23 14:54:10 +0900

    Fix DI

---
+++ app/src/main/java/com/hiring/cleanarchitecture/di/PresentationModules.kt
@@ -0,0 +1,36 @@
+package com.hiring.cleanarchitecture.di
+
+import com.hiring.cleanarchitecture.view.ViewModelArgs
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+    @Singleton
+    @Provides
+    @Named("DispatchersIO")
+    fun provideDispatchersIO(): CoroutineDispatcher {
+        return Dispatchers.IO
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ViewModelModule {
+    @Singleton
+    @Provides
+    fun provideViewModelArgs(
+        @Named("DispatchersIO") dispatcherIO: CoroutineDispatcher
+    ): ViewModelArgs {
+        return ViewModelArgs(
+            dispatcherIO = dispatcherIO
+        )
+    }
+}
